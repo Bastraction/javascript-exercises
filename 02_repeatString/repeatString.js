86f4ab34020cdf2061bfc8af62149eafc053d84f
bastraction <jleerod3@gmail.com>
2024-10-06 18:58:19 -0400

    Left off at exercise 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+
+    if(num < 0){
+        return 'ERROR'
+    }
+
+    str = ''
+    for (let index = 0; index < num; index++) {
+        
+        str = str + string;
+        
+    }
+
+    return str;
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    for (let index = 0; index < string.length; index++) {
+        const element = string[index];
+        
+    }
 };
 
 // Do not edit below this line
